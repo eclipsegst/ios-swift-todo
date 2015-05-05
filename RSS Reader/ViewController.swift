841b69843cfe2faa482984ee65bbf1a772eb37fe
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-05-05 13:49:33 -0500

    initial commit

---
+++ RSS Reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSS Reader
+//
+//  Created by iOS Students on 5/5/15.
+//  Copyright (c) 2015 Zhaolong Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
