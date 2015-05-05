d6f2c15eba97aa165e45c342ce0a1dede8899314
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-05-04 22:05:13 -0500

    initialized and completed ios training

---
+++ README.md
@@ -0,0 +1,2 @@
+# iOS Training
+Using [Start Developing iOS Apps Today](https://developer.apple.com/library/ios/referencelibrary/GettingStarted/RoadMapiOS/)
---
+++ ToDo/ToDoItem.m
@@ -0,0 +1,26 @@
+//
+//  ToDoItem.m
+//  ToDo
+//
+//  Created by iOS Students on 5/4/15.
+//  Copyright (c) 2015 Zhaolong Zhong. All rights reserved.
+//
+
+#import "ToDoItem.h"
+
+@implementation ToDoItem
+
+//- (void)markAsCompleted:(BOOL)isComplete {
+//    self.completed = isComplete;
+//    [self setCompletionDate];
+//}
+//
+//- (void)setCompletionDate {
+//    if (self.completed) {
+//        self.completionDate = [NSDate date];
+//    } else {
+//        self.completionDate = nil;
+//    }
+//}
+
+@end
